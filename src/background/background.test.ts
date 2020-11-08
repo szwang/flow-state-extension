43007ec00f81ab1a595cd16c5c4319a719dc12dd
szwang <suzanneswang@gmail.com>
2020-11-08 08:26:46 -0800

    Add testing config

---
+++ src/background/background.test.ts
@@ -0,0 +1,9 @@
+import { retrieveData } from './background';
+
+test('basic', () => {
+  expect(sum()).toBe(0);
+});
+
+test('basic again', () => {
+  expect(sum(1, 2)).toBe(3);
+});
--- src/background/background.ts
+++ src/background/background.ts
@@ -1,19 +1,30 @@
+// background script for filtering sites
+
+interface LoadingData {
+  sites: Array<string>;
+  endTime: number;
+}
+
+// get sites and endTime from storage
+export function retrieveData(cb: (data: LoadingData) => void) {
+  chrome.storage.sync.get(['sites', 'endTime'], (data: LoadingData) => {
+    cb(data);
+  });
+}
+
 // close tabs
 chrome.tabs.onUpdated.addListener((tabId: number, changeInfo: any) => {
   const url = changeInfo.pendingUrl || changeInfo.url;
   const hostname = new URL(url).hostname;
 
-  chrome.storage.sync.get(
-    ['sites', 'endTime'],
-    ({ sites, endTime }: { sites: Array<string>; endTime: number }) => {
-      if (
-        !sites.find((domain: string) => hostname.includes(domain)) &&
-        endTime > Date.now()
-      ) {
-        chrome.tabs.executeScript(tabId, { file: 'clearPage.bundle.js' });
-      } else {
-        // alert('site is not blocked');
-      }
+  retrieveData(({ sites, endTime }) => {
+    if (
+      !sites.find((domain: string) => hostname.includes(domain)) &&
+      endTime > Date.now()
+    ) {
+      chrome.tabs.executeScript(tabId, { file: 'clearPage.bundle.js' });
+    } else {
+      // alert('site is not blocked');
     }
-  );
+  });
 });
